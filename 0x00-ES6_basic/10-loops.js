e9859d1cee0a50ebc6507dedf15da2092da81847
Bolatito <shorungbebolatito@gmail.com>
2024-10-04 22:53:39 +0100

    loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,10 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArr = [];
 
-  for (const [idx, value] of array.entries()) {
-    // var value = array[idx];
-    newArr[idx] = appendString + value;
+  for (const value of array) {
+    newArr.push(appendString + value);
   }
-
   return newArr;
 }
