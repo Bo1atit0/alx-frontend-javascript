e56635b0a06b2302c9be48f21e5e81cc705a2568
bo1atito <shorungbebolatito@gmail.com>
2025-01-29 21:09:05 +0100

    added 100-await file

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo;
+  let user;
+
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
+}
